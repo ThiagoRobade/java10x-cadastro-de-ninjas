cfd081207c1f65a874d60d923e60f99bc73f29c1
ThiagoRobade <robade@live.com>
2025-07-16 21:49:45 -0300

    iniciando o projeto cadastro de ninjas

---
+++ src/main/java/dev/java10x/cadastrodeninjas/Java10xApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java10xApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java10xApplication.class, args);
+    }
+
+}
