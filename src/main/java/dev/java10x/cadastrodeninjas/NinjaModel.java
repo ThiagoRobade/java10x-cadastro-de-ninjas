c14eb0a90279de8749b95ddddb1b21375d1b66bc
ThiagoRobade <robade@live.com>
2025-07-21 17:20:59 -0300

    cria entidade MissoesModel e relacionamento OneToMany

--- src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
@@ -11,9 +11,16 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     String nome;
+
     String email;
+
     int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
 
     // No Args Constructor
     public NinjaModel() {
@@ -23,6 +30,40 @@ public class NinjaModel {
     public NinjaModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
+        this.idade = idade;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 }
